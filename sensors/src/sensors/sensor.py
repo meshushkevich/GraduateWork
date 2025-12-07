f3ba2e454ae0d29d8dcecc4339b47b8fb66ccecd
Maksim Shushkevich <m.e.shushkevich@yandex.com>
2025-12-07 17:54:22 +0300

    Add Ci/Cd

--- sensors/src/sensors/sensor.py
+++ sensors/src/sensors/sensor.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
 from datetime import datetime
-from typing import Any, Dict, Optional
+from typing import Any, Dict
 
 
 @dataclass
